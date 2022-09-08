d8dfe309384fac0794911bc734dadecb053622cf
Temijoe20 <abijo356@gmail>
2022-09-08 12:13:02 -0700

    Where you dey

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ *main- Servesas the entry point of the program
+ *
+ *Return: By default returns zero if no errors otherwise returns non-zero value.*/
+int main (void)
+{
+ puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+} 
