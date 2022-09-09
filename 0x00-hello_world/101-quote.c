5480d26b8e3260826c71b723e8c7bc762c2f4e76
Temijoe20 <abijo356@gmail>
2022-09-08 21:10:55 -0700

    fresh guy

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,10 @@
-#include <unistd.h>
+#include <unistd>
 /**
- *main-program entry point.
+ * main - program entry point
  *
- * Return: 1 on success.
+ * Return: 1
  */
 int main(void)
-{write(2,"and that piece of art is useful\" - Dora Korpar,2015-10-19\n",59);
-	return(1);
-}
+{
+	write(2, "and that piece of art is useful\" -Dora Korpar,2015-10-19\n",59);
+	return (1);
