bf296d76cdbf28df6536fc88f296116d6296e75a
Temijoe20 <abijo356@gmail>
2022-09-08 17:38:40 -0700

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,11 @@
+#include <stdio.h>
 /**
-* main - print the string in the put function
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
-* Return: 0
-*/
+ *main- Serves as the entry point for the program
+ *
+ *Return: By default returns zero if no erros otherwise returns non-zero value
+ */
 int main(void)
 {
-puts("\"Programming is like bu
-
-la
-
-
-
-
-
-
-cd
+	puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}		
