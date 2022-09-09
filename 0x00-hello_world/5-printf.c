5301ecb6dfbc0b4a398cbe11f31dc5dc75e42fe5
Temijoe20 <abijo356@gmail>
2022-09-08 17:48:25 -0700

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
-#include <studio.h>
-
+#include <stdio.h>
 /**
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints "Programming i like buildng a multilingual puzzle
-* Return: 0
-*/
+ *  * main-entry point for the program.
+ *   *
+ *    * Return:0 if no errors, return non zero if errors
+ *     */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
-	return (0);
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
 }
