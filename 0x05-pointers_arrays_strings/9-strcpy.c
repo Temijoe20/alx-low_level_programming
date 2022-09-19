387f54c9e68e0027f95b93fa82dcd3f13c09395a
Temijoe20 <abijo356@gmail>
2022-09-19 16:11:28 -0700

    try

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+_strcpy - copy paste string
+*edest: destination
+@src: source
+*
+* Return: destination
+*/
+
+char *_strcpy(char *dest, char *src)
+{
+		int inc = 0;
+
+		while (*(src + inc) != '\0')
+		{
+			*(dest + inc) = *(src + inc);
+			inc++;
+		}
+		*(dest + inc) = '\0';
+
+		return (dest);
+}
