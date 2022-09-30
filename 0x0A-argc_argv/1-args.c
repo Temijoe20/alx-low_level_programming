b2b5dc8f961d8525421cc0ebe9ff2215e0af81b3
Temijoe20 <abijo356@gmail>
2022-09-30 10:36:23 -0700

    push

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,7 +4,7 @@
  * main - main entry point of the program
  * @argc: Count of all the command-line arguments
  * @argv: Vector containing all the command-line argumnents
- * 
+ *
  * Description: This program prints out the number of command-line arguments
  * to the screen
  * Return: Always 0 (Successful)
