e0cfaf30cead146829263bcfedad7b3e62c1d63a
Temijoe20 <abijo356@gmail>
2022-09-30 19:44:14 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,59 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <string.h>
-
-int is_num(char *str);
 
 /**
- * main - main entry point for the program
- * @argc: Count of the command line arguments
- * @argv: Vector containing command line arguments
+ * main - adds numbers
+ * @argc: number of arguments passed to the function
+ * @argv: argument vector of pointers to strings
  *
- * Return: 0 or 1
+ * Return: 0 if no errors, else 1
  */
 int main(int argc, char *argv[])
 {
-	int i, num, sum;
+	int a = 0, i, j;
 
-	sum = 0;
 	for (i = 1; i < argc; i++)
 	{
-		if (is_num(argv[i]))
+		for (j = 0; argv[i][j]; j++)
 		{
-			num = atoi(argv[i]);
-			if (num > 0)
-				sum += num;
+			if (isdigit(argv[i][j]) == 0)
+			{
+				puts("Error");
+				return (1);
+			}
 		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
-	{
-		printf("%d\n", sum);
-
-		return (0);
-}
-
-/**
- * is_num - determines if a string is an integer
- * @str: string to be examined
- *
- * Return: 0 or 1
- */
-int is_num(char *str)
-{
-	unsigned int i;
-
-	i = 0;
-	while (i < strlen(str))
-	{
-		if (isdigit(str[i]) == 0)
-		{
-			return (0);
-		}
-		i++;
 	}
-	return (1);
+	for (i = 1; i < argc; i++)
+	{
+		a += atoi(argv[i]);
+	}
+	printf("%d\n", a);
+	return (0);
 }
