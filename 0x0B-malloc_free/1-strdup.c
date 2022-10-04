bd673123d6f4eed54dac8a88a50a62ab20add596
Temijoe20 <abijo356@gmail>
2022-10-04 08:50:29 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,34 +1,59 @@
 #include "main.h"
 #include <stdlib.h>
 
+unsigned int get_length(char *s);
+
 /**
- * _strdup - a function that copies a string
- * @str: the string to  be copied
- * Return: returns a pointer to a newly allocated space in memonry
- * which contains a copy of the given string
+ * _strdup - copies the contents of a string to another location in memory
+ * @str: String to copy
+ *
+ * Return: 0 or Ptr
  */
 char *_strdup(char *str)
 {
-	int i;
-	int c = 0;
-	char *newStr;
+	char *ptr;
+	unsigned int i;
+	unsigned int size;
 
-	if (str == NULL)
-		return (NULL);
-	for (i = 0 ; ; i++)
+	if (str == 0)
 	{
-		c++;
-		if (str[i] == '\0')
-			break;
+		return (0);
 	}
 
-	newStr = (char *) malloc(c * sizeof(char));
-	if (newStr == NULL)
-		return (NULL);
-
-	for (i = 0; i < c; i++)
+	size = get_length(str);
+	ptr = (char *) malloc((sizeof(char) * size) + 1);
+	if (ptr == 0)
 	{
-		newStr[i] = str[i];
+		return (0);
 	}
-	return (newStr);
+
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = str[i];
+	}
+	ptr[size] = '\0';
+
+	return (ptr);
+}
+
+/**
+ * get_length - returns the length of the string
+ * @s: string parameter
+ *
+ * Return: length of s
+ */
+unsigned int get_length(char *s)
+{
+	unsigned int length;
+	unsigned int i;
+
+	i = 0;
+	length = 0;
+	while (s[i] != '\0')
+	{
+		length++;
+		i++;
+	}
+
+	return (length);
 }
